8da84ff217ccdd796b7dd58fec275c9a9951b300
Henrique Tavares <ihenrits@gmail.com>
2019-08-01 23:37:03 -0300

    Valiaçao para usuario nao se inscrever em seu proprio meetapp

--- src/app/controllers/MeetappSubscriptionController.js
+++ src/app/controllers/MeetappSubscriptionController.js
@@ -5,6 +5,14 @@ import Meetapp from '../models/Meetapp';
 
 class MeetappSubscriptionController {
   async store(req, res) {
+    const meetapp = await Meetapp.findOne({ where: { id: req.params.id } });
+
+    if (req.userId === meetapp.user_id) {
+      return res.status(400).json({
+        error: 'You cannot register for an meetapp that you create.',
+      });
+    }
+
     const { id, user_id, meetapp_id } = MeetappSubscription.create({
       user_id: req.userId,
       meetapp_id: req.params.id,
